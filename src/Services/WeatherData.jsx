3e9d36bbfe2cd3d3757ec17160b2588b98a2f2cf
Vraj Patel <vraj.patel.12390@gmail.com>
2023-10-26 12:51:18 -0400

    Context is created so that I can use weather data in child component

---
+++ src/Services/WeatherData.jsx
@@ -0,0 +1,7 @@
+// The below code exports a variable named weather_data which is initialized with null. This variable is created using the createContext function from the React library. The createContext function creates a new context object that can be used to share data between components in a React application.
+
+import { createContext } from "react";
+
+const weather_data = createContext([]);
+
+export default weather_data;
